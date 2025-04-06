124a60710c49acf9c2d74ea7844e2e6fd48fe08e
Daniel Kozmacs <daniel.kozmacs@digitalnatives.hu>
2025-04-06 15:03:45 +0200

    chore: add credo

--- mix.exs
+++ mix.exs
@@ -22,9 +22,8 @@ defmodule Checkout.MixProject do
   defp deps do
     [
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:decimal, "~> 2.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:decimal, "~> 2.0"},
+      {:credo, "~> 1.7.8", only: [:dev, :test], runtime: false}
     ]
   end
 end
